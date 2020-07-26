586847732858e8b01a7771d405255074120825ca
Paul Müller <dev@craban.de>
2020-07-26 23:17:16 +0200

    set master

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = ['sphinx.ext.intersphinx',
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
